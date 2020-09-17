c00569b7c9beb5d00072988dd7595a637c6022b3
chief <69991451+Josh-chief@users.noreply.github.com>
2020-09-17 17:36:11 +0300

    Add files via upload

---
+++ Question 4/src/question/pkg4/Question4.java
@@ -0,0 +1,25 @@
+
+package question.pkg4;
+
+
+public class Question4 {
+
+   
+    public static void main(String[] args) {
+       int n = 100;
+       int sum =0;
+       
+	System.out.println("Odd Numbers from 0 to "+n+" are: ");
+	for (int i = 1; i <= n; i++) {
+	   if (i % 2 != 0) ;
+                  
+	System.out. println(i + " ");
+           sum= sum+i;
+        }
+       System.out. println("The sum of odd number is " +  sum);
+	}
+   }
+
+    
+    
+
